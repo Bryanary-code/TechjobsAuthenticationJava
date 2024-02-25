5882bffe44ff42df1ebd8157267d7f49054252d5
Bryan Wulfert <bwulfert@gmail.com>
2024-02-25 14:52:18 -0600

    Auth studio complete

---
+++ src/main/java/org/launchcode/techjobsauth/models/User.java
@@ -0,0 +1,32 @@
+package org.launchcode.techjobsauth.models;
+
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Entity
+public class User extends AbstractEntity{
+    @NotNull
+    private String username;
+    @NotNull
+    private String pwHash;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.pwHash = encoder.encode(password);
+    }
+
+    public User() {
+
+    }
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
